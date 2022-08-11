78cd96d672a0f891399476fb33b0a9bd8ee3b299
KayaTaylan <taylankaya2@outlook.de>
2022-08-11 16:22:54 +0200

    Server & Client Prototype
    Allows for one-sided communication between the Client and the Server

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+
+connected = True
+print("You can easily disconnect from the Server with 'Q'")
+while(connected):
+    msg = input()
+    if(msg == 'Q' or msg == 'q'):
+        connected = False
+    else:
+        send(msg)
+
+send(DISCONNECT_MSG)
+
+    
+
